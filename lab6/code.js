b593ae23ccb3db6a07b10eaa7eac02bd9f4764f9
Terence Grigoruk <terence.grigoruk@gmail.com>
2022-02-23 14:35:38 -0800

    completed calculator

--- lab6/code.js
+++ lab6/code.js
@@ -1,67 +1,38 @@
-let history = ''
 
-function add() {
-    x = parseInt(jQuery('#x').val());
-    y = parseInt(jQuery('#y').val());
-    expression = x + ' + ' + y + ' = ' + (x + y)
+let history = []
+function operate(op) {
+    x = parseInt($('#x').val());
+    y = parseInt($('#y').val());
+    switch(op) {
+        case 'add':
+            expression = x + ' + ' + y + ' = ' + (x + y);
+            break;
+        case 'sub':
+            expression = x + ' - ' + y + ' = ' + (x - y);
+            break;
+        case 'mul':
+            expression = x + ' * ' + y + ' = ' + (x * y);
+            break;
+        case 'div':
+            expression = x + ' / ' + y + ' = ' + (x / y);
+            break;
+        case 'exp':
+            expression = x + ' ^ ' + y + ' = ' + (x ** y);
+            break;
+        case 'mod':
+            expression = x + ' % ' + y + ' = ' + (x % y);
+            break;
+        default:
+            expression = 'ERROR'
+            console.log("Error in calculation")
+    }
     jQuery('#result').html("Result of " + expression);
-    history = '<div class="red">' + expression + '</div>' + history;
+    history.unshift(`<div class=${op}-color>${expression}</div>`);
     $('#history').html(history);
 }
-function subtract() {
-    x = parseInt(jQuery('#x').val());
-    y = parseInt(jQuery('#y').val());
-    expression = x + ' - ' + y + ' = ' + (x - y)
-    jQuery('#result').html("Result of " + expression);
-}
-function multiply() {
-    x = parseInt(jQuery('#x').val());
-    y = parseInt(jQuery('#y').val());
-    expression = x + ' * ' + y + ' = ' + (x * y)
-    jQuery('#result').html("Result of " + expression);
-}
-function divide() {
-    x = parseInt(jQuery('#x').val());
-    y = parseInt(jQuery('#y').val());
-    expression = x + ' / ' + y + ' = ' + (x / y)
-    jQuery('#result').html("Result of " + expression);
-}
-
-
-// function operate(op) {
-//     x = parseInt(jQuery('#x').val());
-//     y = parseInt(jQuery('#y').val());
-//     let expression = ''
-//     switch(op) {
-//         case 'add':
-//             expression = x + ' + ' + y + ' = ' + (x + y);
-//             break;
-//         case 'sub':
-//             expression = x + ' - ' + y + ' = ' + (x - y);
-//             break;
-//         case 'mul':
-//             expression = x + ' * ' + y + ' = ' + (x * y);
-//             break;
-//         case 'div':
-//             expression = x + ' / ' + y + ' = ' + (x / y);
-//     }
-//     jQuery('#result').html("Result of " + expression);
-//     // history.unshift(expression + '\n');
-//     // $('#history').html(history);
-// }
-
 
 function setup() {
-    jQuery('#add').click(add);
-    jQuery('#sub').click(subtract);
-    jQuery('#mul').click(multiply);
-    jQuery('#div').click(divide);
-    
-    // jQuery(".operator").click(
-    //     function(){
-    //         operate()
-    //     }
-    // )
+    $(".operator").click(function(){operate(this.id)} )
 }
 
-jQuery(document).ready(setup);
+$(document).ready(setup);
