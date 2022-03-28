885d58666135739375ecea3ea2451d8f1d454c22
tgrigoruk <terence.grigoruk@gmail.com>
2022-03-27 19:03:16 -0700

    completed A2

--- assignment2/code-a2.js
+++ assignment2/code-a2.js
@@ -4,36 +4,36 @@ var currPage = 1;
 var pageSize = 3;
 var numberOfPageButtons = 5;
 
-
 function getMovieData() {
   currPage = 1;
-  displayPageNum(currPage);
   movie = $("#movie-title").val();
   $.ajax({
     url: `https://api.themoviedb.org/3/search/movie?api_key=bab41f87cd616dd4eb77708d7f29e2dc&query=${movie}`,
     type: "GET",
     success: (data) => {
-      $("#movies-go-here").empty();
+      $("#movies-container").empty();
       movies = data.results;
+      // console.log('data results ', movies);
       currentNumberOfPages = Math.ceil(movies.length / pageSize);
-      if (currentNumberOfPages) {
+      if (movies.length) {
         currPage = 1;
         updatePageButtons(currPage);
+        $("#no-movies-found").hide();
         $("#page-buttons-container").show();
         displayMovies(currPage);
       } else {
+        $("#bd-image").attr("src", "");
         $("#page-buttons-container").hide();
-        $("#display-page-num").text("No movies found!");
+        $("#no-movies-found").show();
       }
     },
   });
 }
 
 function displayMovies(page) {
-  // console.log(movies);
-  $("#movies-go-here").empty();
+  $("#movies-container").empty();
   firstMovieInPageIndex = (page - 1) * pageSize;
-  lasttMovieInPageIndex = Math.min(firstMovieInPageIndex + pageSize);
+  lasttMovieInPageIndex = Math.min(firstMovieInPageIndex + pageSize, movies.length);
   for (let i = firstMovieInPageIndex; i < lasttMovieInPageIndex; i++) {
     createCardFromTemplate(movies, i);
   }
@@ -44,52 +44,59 @@ function createCardFromTemplate(movies, index) {
   let newcard = cardTemplate.content.cloneNode(true);
   newcard.querySelector(".title").innerHTML = movies[index].original_title;
   newcard.querySelector(".overview").innerHTML = movies[index].overview;
-  newcard.querySelector(".poster").src =
-    "https://image.tmdb.org/t/p/w200" + movies[index].poster_path;
-  // give unique id to each movie
+  if (movies[index].poster_path) {
+    poster = "https://image.tmdb.org/t/p/w200" + movies[index].poster_path;
+  } else {
+    poster = "https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg"
+  }
+  newcard.querySelector(".poster").src = poster;
   let movie_id = "movie-" + index;
   newcard.querySelector(".movie-card").id = movie_id;
-  document.getElementById("movies-go-here").appendChild(newcard);
-  // can't figure out how to change this attr beforehand!
-  $("#" + movie_id).attr(
-    "backdrop",
-    "https://image.tmdb.org/t/p/original" + movies[index].backdrop_path
-  );
+  document.getElementById("movies-container").appendChild(newcard);
+  if (movies[index].backdrop_path) {
+    $("#" + movie_id).attr(
+      "backdrop",
+      "https://image.tmdb.org/t/p/original" + movies[index].backdrop_path
+    );
+  } else {
+    $("#" + movie_id).attr("backdrop", "");
+  }
   $(".movie-card").click(function () {
     $("#bd-image").attr("src", $(this).attr("backdrop"));
   });
 }
 
-function displayPageNum(num) {
-  $("#display-page-num").text("Page " + num);
-}
 
 function updatePageButtons(page) {
   $("#numbered-buttons").empty();
   let minPage = Math.max(1, page - Math.floor(numberOfPageButtons / 2));
-  maxPage = currentNumberOfPages - numberOfPageButtons;
-  minPage = Math.min(minPage, maxPage);
-  minPage > 1 ? $("#first, #prev").show() : $("#first, #prev").hide();
-  minPage + numberOfPageButtons == currentNumberOfPages ? $("#last, #next").hide() : $("#last, #next").show();
-
-  for (let i = minPage; i < minPage + numberOfPageButtons; i++) {
+  minPage = Math.min(minPage, currentNumberOfPages - numberOfPageButtons + 1);
+  minPage = Math.max(1, minPage);
+  minPage > 1 ? $("#first").show() : $("#first").hide();
+  minPage > currentNumberOfPages - numberOfPageButtons ? $("#last").hide() : $("#last").show();
+  currPage == 1 ? $("#prev").hide() : $("#prev").show();
+  currPage == currentNumberOfPages ? $("#next").hide() : $("#next").show();
+  actualNumberOfPageButtons = Math.min(numberOfPageButtons, currentNumberOfPages);
+  for (let i = minPage; i < minPage + actualNumberOfPageButtons; i++) {
     $("#numbered-buttons").append(
-      `<button class="page-button">${i}</button>`
+      `<button class="page-button" id="p${i}">${i}</button>`
     );
+    $(`#p${i}`).click(changePage);
+    if (i == currPage) {
+      $(`button:contains(${i})`).css("background-color", "rgb(95, 187, 218)");
+    }
   }
-  $(".page-button").click(changePage);
 }
 
 
 function changePage() {
-  let chosenPage = this.innerHTML;
+  let pageButtonPressed = this.innerHTML;
   let prevPage = currPage;
-  if (!isNaN(chosenPage)) {
-    currPage = parseInt(chosenPage);
+  if (!isNaN(pageButtonPressed)) {
+    currPage = parseInt(pageButtonPressed);
   } else {
-    switch (chosenPage) {
+    switch (pageButtonPressed) {
       case "&gt;&gt;":
-        console.log("next");
         currPage++;
         break;
       case "&lt;&lt;":
@@ -104,31 +111,28 @@ function changePage() {
     }
   }
   currPage = Math.max(1, currPage);
-  currPage = Math.min(currPage, currentNumberOfPages - 1);
+  currPage = Math.min(currPage, currentNumberOfPages);
   if (currPage != prevPage) {
     updatePageButtons(currPage);
-    displayPageNum(currPage);
     displayMovies(currPage);
   }
 }
 
 
 function pageSizeSelected() {
+  let firstMovie = (currPage - 1) * pageSize;
   pageSize = parseInt(this.value);
   currentNumberOfPages = Math.ceil(movies.length / pageSize);
-  // console.log(pageSize, currentNumberOfPages);
-  // find movie index, ensure on right page to display first movie on page
-
-  updatePageButtons();// BROKEN!
-  
-  // displayMovies(???);
+  currPage = Math.floor(firstMovie / pageSize) + 1;
+  updatePageButtons(currPage);
+  displayMovies(currPage);
 }
+
 function setup() {
   $("#numbered-buttons").css("grid-template-columns", `repeat(${numberOfPageButtons}, 1fr)`);
   $("#get-movie").click(getMovieData);
+  $("#page-buttons-container, #first, #prev, #next, #last, #no-movies-found").hide();
   $(".page-button").click(changePage);
-  $("#page-buttons-container").hide();
-  $("#first, #prev").hide();
   $("#page-size").change(pageSizeSelected);
 }
 $(document).ready(setup);
